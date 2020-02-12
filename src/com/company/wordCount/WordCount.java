cc571b6ce1603c968d9319b1bdf468a7b915deb3
xiaozhuanfeng <627794853@qq.com>
2020-02-12 13:31:52 +0800

    去掉冗余引用类

--- src/com/company/wordCount/WordCount.java
+++ src/com/company/wordCount/WordCount.java
@@ -9,7 +9,6 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.mapreduce.lib.output.LazyOutputFormat;
 
 import java.io.IOException;
 import java.util.StringTokenizer;
